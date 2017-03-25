d723c60c11493f4d1a117b1b14071ae51479d6a0
ejm714 <ejm714@gmail.com>
2017-03-24 20:18:01 -0700

    q1

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+# Q1
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/ejm714/dsp/master/python/faculty.csv')
+print(df.columns.tolist())
+df.columns = df.columns.str.strip()
+degrees = df['degree'].str.strip().str.replace('.', '')
+counts = Counter((',').join(degrees).replace(' ', ',').split(','))
+len(counts)
