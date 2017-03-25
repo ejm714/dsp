933771634dfc67964ea449b9238bb35133cfb1be
ejm714 <ejm714@gmail.com>
2017-03-24 20:38:16 -0700

    q2

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,9 +1,17 @@
-# Q1
+## OR
+
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/ejm714/dsp/master/python/faculty.csv')
 print(df.columns.tolist())
 df.columns = df.columns.str.strip()
 degrees = df['degree'].str.strip().str.replace('.', '')
-counts = Counter((',').join(degrees).replace(' ', ',').split(','))
-print counts
-len(counts)
+degree_counts = Counter((',').join(degrees).replace(' ', ',').split(','))
+print degree_counts
+len(degree_counts)
+
+
+## Q2
+title = df['title'].str.replace(' is ', ' of ')
+title_counts = title.value_counts()
+print title_counts
+len(title_counts)
