9da231433362d6f5426328f950b580b393294b64
ejm714 <ejm714@gmail.com>
2017-03-24 21:05:41 -0700

    q3 and some updates to q1

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,10 +1,12 @@
 ## Q1
 import pandas as pd
+from collections import Counter
+
 df = pd.read_csv('https://raw.githubusercontent.com/ejm714/dsp/master/python/faculty.csv')
 print(df.columns.tolist())
 df.columns = df.columns.str.strip()
-degrees = df['degree'].str.strip().str.replace('.', '')
-degree_counts = Counter((',').join(degrees).replace(' ', ',').split(','))
+degrees = df['degree'].str.strip().str.replace('.', '').str.split()
+degree_counts = Counter(degrees.sum())
 print degree_counts
 len(degree_counts)
 
@@ -14,3 +16,10 @@ title = df['title'].str.replace(' is ', ' of ')
 title_counts = title.value_counts()
 print title_counts
 len(title_counts)
+
+## Q3
+emails = df['email'].tolist()
+print emails
+txt_file = open('q3.txt', 'w')
+txt_file.write('%s' % emails)
+txt_file.close()
